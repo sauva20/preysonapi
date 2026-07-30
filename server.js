3986513698c9d59fb70fb68ea3743f015e08efac
sauva20 <2qistysauva@gmail.com>
2026-07-30 21:28:08 +0700

    api fix

--- server.js
+++ server.js
@@ -1557,7 +1557,7 @@ app.use((req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
