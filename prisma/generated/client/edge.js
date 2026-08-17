68e68e8c031393ecef2b9bd698d947194805d5a8
Prabu <prabuxmomo@gmail.com>
2026-08-17 12:25:01 +0700

    ngepush fix

--- prisma/generated/client/edge.js
+++ prisma/generated/client/edge.js
@@ -345,6 +345,7 @@ const config = {
     "db"
   ],
   "activeProvider": "mysql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- prisma/generated/client/index.js
+++ prisma/generated/client/index.js
@@ -346,6 +346,7 @@ const config = {
     "db"
   ],
   "activeProvider": "mysql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
